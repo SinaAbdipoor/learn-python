d3eedd0efbdfc7f38658e96647d74b99c8fdb831
Sina Abdipoor <62741992+SinaAbdipoor@users.noreply.github.com>
2024-09-24 15:07:40 +0800

    Create 02-numbers.py

---
+++ 02-numbers.py
@@ -0,0 +1,12 @@
+print(2 + 2)
+print(2 - 2)
+print(2 * 2)
+print(2 / 2)
+
+number1 = 1
+number2 = 2
+print(number1 + number2)
+print(number1, "+", number2, "=", (number1 + number2))
+
+result = number1 + number2
+print("Result:", result)
